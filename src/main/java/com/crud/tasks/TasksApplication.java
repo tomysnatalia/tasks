221ffefa5e22ac64ca4d603e1257819a4a7cfa68
tomysnatalia <hanf.natalia@gmail.com>
2020-04-26 19:50:48 +0200

    JAR and PostgresSQL fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
