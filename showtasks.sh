82642cc682aad14249e705b6e368064459f5aabc
tomysnatalia <hanf.natalia@gmail.com>
2020-04-26 19:05:32 +0200

    First Commit

--- showtasks.sh
+++ showtasks.sh
@@ -12,7 +12,7 @@ echo "Work is finished"
 
 
 go_to_website () {
- if open http://localhost:8080/tasks; then
+ if open http://localhost:8080/crud/v1/task/getTasks; then
    echo "Website is loading"
  else
   echo "Sorry. Something goes wrong"
