7b796903eb282fc8c2783baf5a9ecf5f9ad85e19
tomysnatalia <hanf.natalia@gmail.com>
2020-07-21 12:26:35 +0200

    commit

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -37,7 +37,7 @@ public class SimpleEmailService {
             MimeMessageHelper messageHelper= new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(mail.getMailTo());
             messageHelper.setSubject(mail.getSubject());
-            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+            messageHelper.setText(mail.getMessage(), true);
         };
     }
 
